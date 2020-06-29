2ee68c265765ab801b0744b34c7c9361670a7543
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-06-28 23:27:36 -0500

    add about skill

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './components/commons/Header';
 import Footer from './components/commons/Footer';
 import SocialNets from './components/SocialNets';
+import About from './components/About';
 
 function App() {
   return (
     <div className="container">
        <Header/>
        <SocialNets/>
+       <section>
+         <About/>
+       </section>
        <Footer/>
     </div>
   );
