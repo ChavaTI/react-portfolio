91c81e5bd0935787699a6e74e04082d0656e08bb
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-09-25 00:21:39 -0500

    Apply format

--- src/components/AboutSkill.jsx
+++ src/components/AboutSkill.jsx
@@ -3,17 +3,19 @@ import React from "react";
 const AboutSkill = ({currentSkill}) => {
   return (
     currentSkill === undefined ? null : 
-    <div id="about-skill" class="about-skill">
-       <div class="card">
-          <div class="card-header">
+    <div id="about-skill" className = "about-skill">
+       <div className = "card">
+          <div className = "card-header">
               {currentSkill.name}
            </div>
-        <div class="card-about-icon"></div>
-        <div class="card-body">
+        <div className = "card-about-icon">
+          <i className = { currentSkill.icon }></i>
+        </div>
+        <div className="card-body">
           <p>{currentSkill.description}</p>
         </div>
-        <div class="arrow-up" id="back-to-skills">
-          <i class="fas fa-arrow-alt-circle-up"></i>
+        <div className = "arrow-up" id="back-to-skills">
+          <i className = "fas fa-arrow-alt-circle-up"></i>
         </div>
        </div>
     </div>
