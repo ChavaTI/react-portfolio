260db3d07b12d3be3963aaf71e1f64b88db3469d
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-10-04 22:15:21 -0500

    create Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => (
+  <div className = 'card' id = 'contact'>
+   <div className = 'card-header'>Contact</div>
+   <div className = 'card-body contact-body'>
+     <p>Cellphone: <a href="tel:+523513813013">+52 351 381 3013</a></p>
+     <p>Email: <a href="mailto:chavatloz@gmail.com">chavatloz@gmail.com</a></p>
+  </div>
+  </div>
+);
+
+export default Contact;
