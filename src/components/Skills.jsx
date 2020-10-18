13bf806ddc842e5c1ca07fc15bd4188a06d66b7c
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-10-18 00:24:42 -0500

    Fix the bug when skill is unselected don't scroll

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { scroller as scroll  } from 'react-scroll';
+import { scroller as scroll } from "react-scroll";
 import Skill from "./Skill";
 
-const Skills = ({handleCurrentSkill}) => {
+const Skills = ({ handleCurrentSkill }) => {
   const [skills, handleSkills] = useState([
     {
       name: "React",
@@ -13,7 +13,7 @@ const Skills = ({handleCurrentSkill}) => {
       card: "react-card",
       description: `ReactJs is my favorite frontend library develop. I fully know how to use both the props and class components, 
                     stateless functional components and hook components. I’m also a person who likes following good practices. 
-                    I’m skilled at using Redux in ReactJS. When I use this tool I usually connect the react app with the API Rest using axios.`
+                    I’m skilled at using Redux in ReactJS. When I use this tool I usually connect the react app with the API Rest using axios.`,
     },
     {
       name: "Angular",
@@ -24,7 +24,7 @@ const Skills = ({handleCurrentSkill}) => {
       card: "angular-card",
       description: `Angular was the first frontend framework that I knew.  I master using the components of this framework and 
                     I alway try to take advantage of the angular materials to have good UI results. Also, 
-                    I always stick to good practices when I code in TypeScript and Angular.`
+                    I always stick to good practices when I code in TypeScript and Angular.`,
     },
     {
       name: "Node",
@@ -36,7 +36,7 @@ const Skills = ({handleCurrentSkill}) => {
       description: `NodeJS was the first backend framework I’ve ever worked with. At the beginning I used to develop web apps using 
                     NodeJS Express and Jade. But, nowadays I’ve tended to make API rests in order to make MEAN stack apps and MERN stack apps. 
                     The data bases that I’ve used are MySQL, MariaBD and Mongoo.  Consequently, I’ve learned how to connect them with Node apps. 
-                    And Finally, the technology that I use to make authorization tokens is JWT.`
+                    And Finally, the technology that I use to make authorization tokens is JWT.`,
     },
     {
       name: "PHP",
@@ -46,7 +46,7 @@ const Skills = ({handleCurrentSkill}) => {
       selected: false,
       card: "php-card",
       description: `My first web language was PHP, I’ve used this language since university. My knowledge of this field goes from  PHP 7 on web server, 
-      connecting the app with MySQL and SQLServer databases, to how to print the data in order`
+      connecting the app with MySQL and SQLServer databases, to how to print the data in order`,
     },
     {
       name: "Laravel",
@@ -56,7 +56,7 @@ const Skills = ({handleCurrentSkill}) => {
       selected: false,
       card: "laravel-card",
       description: `I've created project with Laravel framework using its tools like Laravel controllers, blade, eloquent, phpunit for test the code and 
-                    anothers console tools to created the basic code and save time in the development, I've used migrations to mkae changes in mysql database.`
+                    anothers console tools to created the basic code and save time in the development, I've used migrations to mkae changes in mysql database.`,
     },
     {
       name: "Java",
@@ -68,7 +68,7 @@ const Skills = ({handleCurrentSkill}) => {
       description: `Java is my first POO language. I have a lot of experience coding in Java since I’ve worked with it for a long time. 
                     I know  how to make either CLI apps, GUI apps (using code and using NetBeans tools) and web apps. 
                     Another thing that I’d like to point out is that I've used many libraries and frameworks in java such that JDBC, JDBI, JPA, 
-                    Hibernate, SpringMVC.`
+                    Hibernate, SpringMVC.`,
     },
     {
       name: "DB",
@@ -80,7 +80,7 @@ const Skills = ({handleCurrentSkill}) => {
       description: `One element inside my skill set is designing SQL databases. The common way that I follow to achieve this task is 
                     I start modeling using CASE tools and then I implement them in MySQL , MariaDB or SQLServer. 
                     I know how to manage the databases and add security. I face all the above not only using tools as PhpMyAdmin and MySql Workbench 
-                    but using SQL script too.  Within the No SQL databases I’ve learned to create Mongo databases.`
+                    but using SQL script too.  Within the No SQL databases I’ve learned to create Mongo databases.`,
     },
     {
       name: "Linux",
@@ -95,7 +95,7 @@ const Skills = ({handleCurrentSkill}) => {
 		    the Apache web server, NGINX, Tomcat server, DNS servers, DHCP servers, Webmin, databases (See more in databases section), and LAMP server. 
                     Even more, I know how to install and use network security tools like Nagios and nmap. 
                     My actual linux system is Arch linux but I've opted for Ubuntu (Kubuntu, Xubuntu, Ubuntu mate), Debian, Fedora, Manjaro, Linux Mint 
-		    as personal systems too. Lastly, writing shell script code is also part of my skill set.`
+		    as personal systems too. Lastly, writing shell script code is also part of my skill set.`,
     },
     {
       name: "Docker",
@@ -108,7 +108,7 @@ const Skills = ({handleCurrentSkill}) => {
                     When I work in team I prefer to use docker so the team can be sure that the project will work in any computer 
                     no matter their Operative System. I master using docker in console and using scripts like dockerfiles and docker-compose. 
                     I’ve launched docker containers with different servers like Apache, Ngnix, and LAMP (mainly Linux, Mysql and PHP). 
-                    There is a bunch of projects in which I have appealed docker, these include, but are not limited to laravel, node, and java projects.`
+                    There is a bunch of projects in which I have appealed docker, these include, but are not limited to laravel, node, and java projects.`,
     },
     {
       name: "Diagram",
@@ -120,7 +120,7 @@ const Skills = ({handleCurrentSkill}) => {
       description: `I’m able to make software engineering that goes from requirements analysis using interviews, surveys, documentation analysis, 
                     until software design using entity–relationship models, relational models, database normalization, flowcharts, use case diagrams, 
                     and mockup designs. In addition, during the develop phase, I have experience working with SCRUM and KANBAN. Finally, 
-                    I've deployed projects on AWS and digital ocean using docker.`
+                    I've deployed projects on AWS and digital ocean using docker.`,
     },
     {
       name: "Language",
@@ -132,7 +132,7 @@ const Skills = ({handleCurrentSkill}) => {
       description: `My native language is spanish and I've studied english since elementary school until now. 
                     I've been in different english courses but I try to practice my english everyday with some friends.
                     I consider that my english is good because apart from I've tried to read more english books to improve,  
-                    I've been able to establish a conversation in english with american people.`
+                    I've been able to establish a conversation in english with american people.`,
     },
     {
       name: "Learning",
@@ -145,7 +145,7 @@ const Skills = ({handleCurrentSkill}) => {
                     Then, I study these topics whenever I need it or I want to learn new informatic stuff. 
 		    I resort to all kind of resources, for instance web sites, blogs, forums, software documentations, tutorials, web courses, or books.
                     I consider myself a self-taught person. If I don't know how to do it, I'll learn it and then I’ll do it. 
-                    It is worth mentioning that I love sharing my knowledge with people, which I think is a great feature of mine.`
+                    It is worth mentioning that I love sharing my knowledge with people, which I think is a great feature of mine.`,
     },
   ]);
 
@@ -153,18 +153,18 @@ const Skills = ({handleCurrentSkill}) => {
     let copySkills = [...skills];
     let updateSkills = copySkills.map((skill) => {
       if (skill.rawName === skillData.rawName) {
-	if(skill.selected){
-	  skill.selected = false;
-	  handleCurrentSkill({});
-	}else{
-	  skill.selected = true;
-	  handleCurrentSkill(skill);
-	}
-	scroll.scrollTo('about-skill', {
-	  smooth: true
-	});
-      }else{
-	skill.selected = false;
+        if (skill.selected) {
+          skill.selected = false;
+          handleCurrentSkill({});
+        } else {
+          skill.selected = true;
+          handleCurrentSkill(skill);
+          scroll.scrollTo("about-skill", {
+            smooth: true,
+          });
+        }
+      } else {
+        skill.selected = false;
       }
       return skill;
     });
@@ -173,12 +173,12 @@ const Skills = ({handleCurrentSkill}) => {
 
   return (
     <div id="skills" className="card">
-    <div className="card-header">Skills</div>
-    <div className="card-body-skills">
-    {skills.map((skill) => (
-      <Skill key={skill.rawName} skill={skill} syncSkills={syncSkills} />
-    ))}
-    </div>
+      <div className="card-header">Skills</div>
+      <div className="card-body-skills">
+        {skills.map((skill) => (
+          <Skill key={skill.rawName} skill={skill} syncSkills={syncSkills} />
+        ))}
+      </div>
     </div>
   );
 };
