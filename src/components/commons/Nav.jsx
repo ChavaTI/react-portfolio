881cacb17b39a8435df2809d15d1f56253c8e053
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-12-08 21:40:55 -0600

    format to Nav.jsx

--- src/components/commons/Nav.jsx
+++ src/components/commons/Nav.jsx
@@ -1,41 +1,43 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 
-const Nav = () => (
-  <nav>
-    <div className="nav-title">
-      <p>My web resume</p>
-    </div>
-    <div className="nav-navigator">
-      <ul>
-        <li>
-          <Link to="about" smooth={true}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="skills" smooth={true}>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link to="portfolio" smooth={true}>
-            Portafolio
-          </Link>
-        </li>
-        <li>
-          <Link to="experience" smooth={true}>
-            Experience
-          </Link>
-        </li>
-        <li>
-          <Link to="contact" smooth={true}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav-title">
+        <p>My web resume</p>
+      </div>
+      <div className="nav-navigator">
+        <ul>
+          <li>
+            <Link to="about" smooth={true}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="portfolio" smooth={true}>
+              Portafolio
+            </Link>
+          </li>
+          <li>
+            <Link to="experience" smooth={true}>
+              Experience
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
