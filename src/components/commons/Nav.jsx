32815daaa9595495fc74922dd33f70175a27bf00
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-12-11 23:55:57 -0600

    off menu if it's clicked

--- src/components/commons/Nav.jsx
+++ src/components/commons/Nav.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 
 const Nav = () => {
+  const [checked, handleChecked] = useState(false);
   return (
     <nav>
-      <input type="checkbox" id="nav-check" />
+      <input
+        type="checkbox"
+        id="nav-check"
+        checked={checked}
+        onChange={(e) => {
+          e.persist();
+          handleChecked(true);
+        }}
+      />
       <div className="nav-header">
         <div className="nav-title">My web resume</div>
       </div>
       <div className="nav-btn">
-        <label className="nav-check" htmlFor = "nav-check">
+        <label className="nav-check" htmlFor="nav-check">
           <span></span>
           <span></span>
           <span></span>
         </label>
       </div>
       <div className="nav-navigator">
-        <Link to="about" smooth={true}>
+        <Link to="about" smooth={true} onClick={() => handleChecked(false)}>
           About
         </Link>
-        <Link to="skills" smooth={true}>
+        <Link to="skills" smooth={true} onClick={() => handleChecked(false)}>
           Skills
         </Link>
-        <Link to="portfolio" smooth={true}>
+        <Link to="portfolio" smooth={true} onClick={() => handleChecked(false)}>
           Portafolio
         </Link>
-        <Link to="experience" smooth={true}>
+        <Link
+          to="experience"
+          smooth={true}
+          onClick={() => handleChecked(false)}
+        >
           Experience
         </Link>
-        <Link to="contact" smooth={true}>
+        <Link to="contact" smooth={true} onClick={() => handleChecked(false)}>
           Contact
         </Link>
       </div>
