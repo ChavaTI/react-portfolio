f278440752f21d5aeb618b98190fe048ff4a1c13
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-12-20 14:16:15 -0600

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import React, { useState, useEffect } from "react";
-import Header from "./components/commons/Header";
-import { scroller as scroll } from "react-scroll";
-import Footer from "./components/commons/Footer";
-import SocialNets from "./components/SocialNets";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import AboutSkill from "./components/AboutSkill";
-import Experiences from "./components/Experiences";
-import Contact from "./components/Contact";
+import React, { useState, useEffect } from 'react';
+import Header from './components/commons/Header';
+import { scroller as scroll } from 'react-scroll';
+import Footer from './components/commons/Footer';
+import SocialNets from './components/SocialNets';
+import About from './components/About';
+import Skills from './components/Skills';
+import AboutSkill from './components/AboutSkill';
+import Experiences from './components/Experiences';
+import Contact from './components/Contact';
+import { FirebaseDatabaseProvider } from '@react-firebase/database';
+import * as firebaseConfig from './firebase/config';
+import firebase from 'firebase';
 
 function App() {
   const [currentSkill, handleCurrentSkill] = useState({});
   useEffect(() => {
-    scroll.scrollTo("about-skill", {
+    scroll.scrollTo('about-skill', {
       smooth: true,
     });
   }, [currentSkill]);
   return (
     <div className="container">
-      <Header />
-      <SocialNets />
-      <section>
-        <About />
-        <Skills handleCurrentSkill={handleCurrentSkill} />
-        <AboutSkill currentSkill={currentSkill} />
-        <Experiences />
-        <Contact />
-      </section>
-      <Footer />
+      <FirebaseDatabaseProvider firebase = {firebase} {...firebaseConfig}>
+        <Header />
+        <SocialNets />
+        <section>
+          <About />
+          <Skills handleCurrentSkill={handleCurrentSkill} />
+          <AboutSkill currentSkill={currentSkill} />
+          <Experiences />
+          <Contact />
+        </section>
+        <Footer />
+      </FirebaseDatabaseProvider>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBZB6Q7s3KIRg9jwpiCBXOfuiAiWDfhors',
+  authDomain: 'resume-63185.firebaseapp.com',
+  projectId: 'resume-63185',
+  storageBucket: 'resume-63185.appspot.com',
+  messagingSenderId: '561864526426',
+  appId: '1:561864526426:web:68d97dd7861b37f7d7e663',
+  measurementId: 'G-TFZJ5SJ7N1',
+};
