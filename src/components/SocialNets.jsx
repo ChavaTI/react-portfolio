e020c892caf814d3a975470cfbee775c9f847863
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-12-31 14:07:49 -0600

    create the service to firestore

--- src/components/SocialNets.jsx
+++ src/components/SocialNets.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import SocialNet from './SocialNet';
-import db from './../firebase/config.js';
+import getData from '../firebase/service.js';
 
 const SocialNets = () => {
   const [socialNets, handleSocialNets] = useState([]);
 
-  const getData = async () => {
-    const fireStoreQuery = db.collection('socialNetworks').get();
-    fireStoreQuery
-      .then((data) => data.docs)
-      .then(docs => docs.map(doc => doc.data()))
-      .then(docsData => {
-	handleSocialNets(docsData);
-      })
-      .catch((error) => console.error(error));
+  const fetchData = async () => {
+    const data = await getData('socialNetworks').then((data) => data);
+    handleSocialNets(data);
   };
   useEffect(() => {
-    getData();
+    fetchData();
   }, []);
   return (
     <div className="social-networks">
